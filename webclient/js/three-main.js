121744477278eebb024fda751f1e6c90be5e74f9
anthony <antonsdl@gmail.com>
2018-01-13 15:54:04 +0300

    THREE added lib and scene with axis

---
+++ webclient/js/three-main.js
@@ -0,0 +1,50 @@
+/**
+ * Created by anthony on 11.01.2018.
+ */
+
+
+const animate = (renderer, scene, camera) => {
+    requestAnimationFrame( animate );
+
+    camera.lookAt(0, 0, 0);
+
+    renderer.render(scene, camera);
+};
+
+
+const main = () => {
+    const scene = new THREE.Scene();
+    const canvas = document.getElementById("glCanvas");
+
+    // define frustum
+    const fov = 75;
+    const zNear = 0.1;
+    const zFar = 1000;
+    const camera = new THREE.PerspectiveCamera(
+        fov,
+        canvas.clientWidth / canvas.clientHeight,
+        zNear,
+        zFar
+    );
+    camera.position.set(2, 3, 15);
+
+
+    const axes = THREE.AxisHelper(30);
+    scene.add(axes);
+
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        antialias: true
+    });
+    renderer.setClearColor(0x333333);
+
+
+    animate(renderer, scene, camera);
+};
+
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    main();
+}, false);
