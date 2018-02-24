c8f19f5b627947d696bd3e68b336a1225ce33ab8
anthony <antonsdl@gmail.com>
2018-02-24 22:34:51 +0300

    SERVER added hack that sets status 200 for each preflight CORS request. no matter it has succeeded or not

--- src/configuration/cors_config.rs
+++ src/configuration/cors_config.rs
@@ -18,13 +18,18 @@ impl Fairing for CORS {
         if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON) {
             response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
             response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS"));
-            response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type, Accept"));
 //            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
         }
 
         if request.method() == Method::Options {
             response.set_header(ContentType::Plain);
             response.set_sized_body(Cursor::new(""));
+
+            if 404 == response.status().code {
+                print!("editing response status of probably failed preflight request");
+                response.set_raw_status(200, "OK");
+            }
         }
     }
 }
