ff2a3bd2fa7cc24eebdad0b7387d7580b8ffbf4b
anthony <antonsdl@gmail.com>
2018-05-30 02:09:12 +0300

    MW added dockerfile. fixed script

--- middleware/middleware.sh
+++ middleware/middleware.sh
@@ -6,4 +6,4 @@ export LOGXI_FORMAT=pretty,happy
 export SELF_PORT=5000
 export TARGET_PORT=8000
 
-go run main.go proxy.go proxy_transport.go
+go run main.go proxy.go proxy_transport.go proxy_message.go
