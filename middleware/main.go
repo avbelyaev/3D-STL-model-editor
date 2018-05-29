accca88e0d308c379770fb81df8417a153dcb3df
anthony <antonsdl@gmail.com>
2018-05-30 00:46:43 +0300

    MW added read ports from env vars

--- middleware/main.go
+++ middleware/main.go
@@ -3,18 +3,42 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"os"
 )
 
+const (
+	ENV_SELF_PORT = "SELF_PORT"
+	ENV_TARGET_PORT = "TARGET_PORT"
+)
+
+
+func getAddr() (string, string) {
+	var selfAddrStr = "localhost:"
+	if "" != os.Getenv(ENV_SELF_PORT) {
+		selfAddrStr += os.Getenv(ENV_SELF_PORT)
+
+	} else {
+		selfAddrStr += "5000"
+	}
+
+	var targetAddrStr = "http://localhost:"
+	if "" != os.Getenv(ENV_TARGET_PORT) {
+		targetAddrStr += os.Getenv(ENV_TARGET_PORT)
+
+	} else {
+		targetAddrStr += "8000"
+	}
+
+	return selfAddrStr, targetAddrStr
+}
+
 func main() {
+	var selfAddr, targetAddr = getAddr()
 
-	// TODO get from ENV vars
-	var proxyAddrStr = "localhost:5000"
-	var targetAddrStr = "http://localhost:8000"
-
-	var proxy = NewCachingProxy(proxyAddrStr, targetAddrStr)
-
+	var proxy = NewCachingProxy(selfAddr, targetAddr)
 	http.HandleFunc("/", proxy.handle)
-	proxy.log.Info(fmt.Sprintf("starting proxy: %s -> %s", proxyAddrStr, targetAddrStr))
+
+	proxy.log.Info(fmt.Sprintf("starting proxy: %s -> %s", selfAddr, targetAddr))
 	proxy.log.Error("proxy listener failed", "error",
-		http.ListenAndServe(proxyAddrStr, nil))
+		http.ListenAndServe(selfAddr, nil))
 }
--- middleware/middleware.sh
+++ middleware/middleware.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+
 export LOGXI=*
 export LOGXI_FORMAT=pretty,happy
+
+export SELF_PORT=5000
+export TARGET_PORT=8000
 
 go run main.go proxy.go proxy_transport.go
