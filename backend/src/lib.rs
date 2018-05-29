064d6c8e48bf359471d6859987715cae57c25110
anthony <antonsdl@gmail.com>
2018-05-30 01:27:40 +0300

    SRV removed bad logger

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -6,9 +6,6 @@ extern crate byteorder;
 extern crate geometry_kernel;
 extern crate regex;
 
-#[macro_use] extern crate log;
-extern crate env_logger;
-
 extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
@@ -18,5 +15,4 @@ extern crate stl;
 pub mod domain;
 pub mod application;
 pub mod port;
-pub mod resource;
 pub mod configuration;
